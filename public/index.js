ffb4ef98ec72b104074fd24b888f1f97fed043fd
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-07-07 21:37:22 +0530

    feat: completed frontend functionality

--- public/index.js
+++ public/index.js
@@ -1,12 +1,36 @@
 // API related functionality
 const baseUrl = "http://localhost:3000";
 
+const uploadFile = async (data) => {
+  try {
+    const options = {
+      method: "POST",
+      headers: [],
+      body: data,
+    };
+    const response = await fetch(`${baseUrl}/api/csv`, options);
+    const readable = await response.json();
+    return readable;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const getFiles = async () => {
   try {
     const response = await fetch(`${baseUrl}/api/csv`);
     const readable = await response.json();
-    // console.log({ readable });
     return readable.files;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getFileData = async (id) => {
+  try {
+    const response = await fetch(`${baseUrl}/api/csv/${id}`);
+    const readable = await response.json();
+    return readable.file;
   } catch (error) {
     console.log(error);
   }
@@ -15,38 +39,41 @@ const getFiles = async () => {
 // DOM related functionality
 document.addEventListener("DOMContentLoaded", async () => {
   const files = await getFiles();
-  console.log({ files });
-  // const files = [
-  //   { name: "file 1", id: "1" },
-  //   { name: "file 2", id: "2" },
-  //   { name: "file 3", id: "3" },
-  //   { name: "file 4", id: "4" },
-  //   { name: "file 5", id: "5" },
-  // ];
 
   const fileUploadFormEl = document.querySelector(".file-upload-form");
-  const fileInputEl = document.querySelector("#file-input");
   const fileListEl = document.querySelector(".files-list-container");
   const tableEl = document.querySelector(".file-data-table-container");
-  console.log(fileInputEl);
 
-  fileUploadFormEl.addEventListener("submit", (event) => {
-    event.preventDefault();
-    console.log(event.target["file-input"].value);
-    console.log(event.target["file-input"].files[0]);
+  fileUploadFormEl.addEventListener("submit", async (event) => {
+    try {
+      event.preventDefault();
+      const formData = new FormData();
+      formData.append("file", event.target["file-input"].files[0]);
+      await uploadFile(formData);
+      createFilesList();
+    } catch (error) {
+      console.log(error);
+    }
   });
 
+  /**
+   * Populates the files list.
+   */
   function createFilesList() {
     try {
       fileListEl.innerHTML = "";
       files.map((file) => {
-        console.log({ file });
-        console.log("id: ", file._id);
-        console.log("name: ", file.fileName);
         const fileBtn = document.createElement("button");
         fileBtn.classList.add("btn", "btn-outline-info");
         fileBtn.setAttribute("id", file._id);
         fileBtn.textContent = file.fileName;
+
+        fileBtn.addEventListener("click", async (event) => {
+          const id = event.target.getAttribute("id");
+          const data = await getFileData(id);
+          createFileDisplayContent(data);
+        });
+
         fileListEl.appendChild(fileBtn);
       });
     } catch (error) {
@@ -54,9 +81,31 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
   }
 
-  createFilesList();
+  function createFileDisplayContent(fileData) {
+    tableEl.innerHTML = "";
+    const headings = Object.keys(fileData.data[0]);
 
-  // function createFileDisplayContent() {
-  //   tableEl.innerHTML = "";
-  // }
+    tableEl.innerHTML = `
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        ${headings.map((row) => `<th scope="col">${row}</th>`).join("")}
+      </tr>
+    </thead>
+    <tbody>
+      ${fileData.data
+        .map(
+          (row, index) =>
+            `<tr>
+          <th scope="row">${index + 1}</th>
+          ${headings.map((heading) => `<td>${row[heading]}</td>`).join("")}
+        </tr>`
+        )
+        .join("")}  
+    </tbody>
+    `;
+  }
+
+  // populating the available files in the list
+  createFilesList();
 });
